2ab6e6802aeedea163a2b614e59de73eef789f72
unknown <MaxwellWang>
2026-02-23 15:27:10 +0800

    加一点hall数据角度判断

--- motor/foc_hall.c
+++ motor/foc_hall.c
@@ -77,7 +77,7 @@ void * HALL_TIMx_CC_IRQHandler( void * pHandleVoid )
         //此时错误不算时间 也不更新速度,下次计数应该用错误计数的值和下次值的累加和
         return; //不计算速度和时间
     }
-    motor->hall_val = hall_val; //输出新的角度
+    motor->hall_val = hall_val; //输出新的角度,主程序中可以知道角度的更新 方便映射电角度
     //这里加入学习模式和正常模式区别
     //学习模式 每次触发正确的变化就学习一次,学习模式正反转都要学习
     //开环时候自动学习?
@@ -86,272 +86,29 @@ void * HALL_TIMx_CC_IRQHandler( void * pHandleVoid )
     //
     int ang_hall_int = conf_now->foc_hall_table[hall_val];	//当前值对应表格位置的角度值
     //正常角度是在 -32768(0x8000) ~ 32767（0x7fff)
+    //本次只更新角度切换点 实际角度在AD中断中去判断
+    //这里只算出当前hall切换点角度和当前hall角速度
+    //不考虑角度不对情况
     if(ang_hall_int<65536){
         if(motor->m_ang_hall_int_prev>65535){
-        //上次是否角度不正常 不正常直接赋值正常角度
+            //上次是否角度不正常 不正常直接赋值正常角度
+            //从误差过来计算角速度时间默认最大
+            motor->angWspeed = MaxAngWSpeed; //第一次默认角速度时间最长? 基本没有补偿
+            m_ang60_intTime = 0;    //重新计时
             motor->m_ang_hall_int_prev  = ang_hall_int; //本次角度直接更新
         }else if(ang_hall_int != motor->m_ang_hall_int_prev){
-        //角度变化不一样
+            //角度变化不一样 上次和本次角度都是有效 才可以更新角度,计算出角速度
+            //    ang/m_ang60_intTime   角度/时间  = 角速度
         }else{
         //角度变化一样 不做处理
         }
         m_ang60_intTime  = 0; //角速度第一次补偿到最慢速度时间
     }else{
         //角度数据不对
-        m_ang60_intTime = 0;
+        m_ang60_intTime = 0;    //重新计时
         motor->m_ang_hall_int_prev = 65536; //给一个错误的角度
         return; //
     }
-    if ( pHandle->SensorIsReliable )
-    {
-        /* A capture event generated this interrupt */
-        bPrevHallState = pHandle->HallState;
-
-        if ( pHandle->SensorPlacement == DEGREES_120 )
-        {
-            pHandle->HallState  = GPIO_IsInputPinSet( pHandle->H3Port, pHandle->H3Pin ) << 2
-                                  | GPIO_IsInputPinSet( pHandle->H2Port, pHandle->H2Pin ) << 1
-                                  | GPIO_IsInputPinSet( pHandle->H1Port, pHandle->H1Pin );
-        }
-        else
-        {
-            pHandle->HallState  = ( GPIO_IsInputPinSet( pHandle->H2Port, pHandle->H2Pin ) ^ 1 ) << 2
-                                  | GPIO_IsInputPinSet( pHandle->H3Port, pHandle->H3Pin ) << 1
-                                  | GPIO_IsInputPinSet( pHandle->H1Port, pHandle->H1Pin );
-        }
-
-        switch ( pHandle->HallState )
-        {
-            case STATE_5:
-                if ( bPrevHallState == STATE_4 )
-                {
-                    pHandle->Direction = POSITIVE;
-                    pHandle->MeasuredElAngle = pHandle->PhaseShift;
-                }
-                else if ( bPrevHallState == STATE_1 )
-                {
-                    pHandle->Direction = NEGATIVE;
-                    pHandle->MeasuredElAngle = ( int16_t )( pHandle->PhaseShift + S16_60_PHASE_SHIFT );
-                }
-                else
-                {
-                }
-
-                break;
-
-            case STATE_1:
-                if ( bPrevHallState == STATE_5 )
-                {
-                    pHandle->Direction = POSITIVE;
-                    pHandle->MeasuredElAngle = pHandle->PhaseShift + S16_60_PHASE_SHIFT;
-                }
-                else if ( bPrevHallState == STATE_3 )
-                {
-                    pHandle->Direction = NEGATIVE;
-                    pHandle->MeasuredElAngle = ( int16_t )( pHandle->PhaseShift + S16_120_PHASE_SHIFT );
-                }
-                else
-                {
-                }
-
-                break;
-
-            case STATE_3:
-                if ( bPrevHallState == STATE_1 )
-                {
-                    pHandle->Direction = POSITIVE;
-                    pHandle->MeasuredElAngle = ( int16_t )( pHandle->PhaseShift + S16_120_PHASE_SHIFT );
-                }
-                else if ( bPrevHallState == STATE_2 )
-                {
-                    pHandle->Direction = NEGATIVE;
-                    pHandle->MeasuredElAngle = ( int16_t )( pHandle->PhaseShift + S16_120_PHASE_SHIFT +
-                                                            S16_60_PHASE_SHIFT );
-                }
-                else
-                {
-                }
-
-                break;
-
-            case STATE_2:
-                if ( bPrevHallState == STATE_3 )
-                {
-                    pHandle->Direction = POSITIVE;
-                    pHandle->MeasuredElAngle = ( int16_t )( pHandle->PhaseShift + S16_120_PHASE_SHIFT
-                                                            + S16_60_PHASE_SHIFT );
-                }
-                else if ( bPrevHallState == STATE_6 )
-                {
-                    pHandle->Direction = NEGATIVE;
-                    pHandle->MeasuredElAngle = ( int16_t )( pHandle->PhaseShift - S16_120_PHASE_SHIFT );
-                }
-                else
-                {
-                }
-
-                break;
-
-            case STATE_6:
-                if ( bPrevHallState == STATE_2 )
-                {
-                    pHandle->Direction = POSITIVE;
-                    pHandle->MeasuredElAngle = ( int16_t )( pHandle->PhaseShift - S16_120_PHASE_SHIFT );
-                }
-                else if ( bPrevHallState == STATE_4 )
-                {
-                    pHandle->Direction = NEGATIVE;
-                    pHandle->MeasuredElAngle = ( int16_t )( pHandle->PhaseShift - S16_60_PHASE_SHIFT );
-                }
-                else
-                {
-                }
-
-                break;
-
-            case STATE_4:
-                if ( bPrevHallState == STATE_6 )
-                {
-                    pHandle->Direction = POSITIVE;
-                    pHandle->MeasuredElAngle = ( int16_t )( pHandle->PhaseShift - S16_60_PHASE_SHIFT );
-                }
-                else if ( bPrevHallState == STATE_5 )
-                {
-                    pHandle->Direction = NEGATIVE;
-                    pHandle->MeasuredElAngle = ( int16_t )( pHandle->PhaseShift );
-                }
-                else
-                {
-                }
-
-                break;
-
-            default:
-                /* Bad hall sensor configutarion so update the speed reliability */
-                pHandle->SensorIsReliable = false;
-
-                break;
-        }
-
-
-#ifdef HALL_MTPA
-        {
-            pHandle->_Super.hElAngle = pHandle->MeasuredElAngle;
-        }
-#endif
-
-        /* Discard first capture */
-        if ( pHandle->FirstCapt == 0u )
-        {
-            pHandle->FirstCapt++;
-            HTU_GetCounter();
-        }
-        else
-        {
-            /* used to validate the average speed measurement */
-            if ( pHandle->BufferFilled < pHandle->SpeedBufferSize )
-            {
-                pHandle->BufferFilled++;
-            }
-
-            /* Store the latest speed acquisition */
-            hHighSpeedCapture = HTU_GetWBRValue(HTU_WBR0);
-            wCaptBuf = ( uint32_t )hHighSpeedCapture;
-            hPrscBuf =  0;
-
-            /* Add the numbers of overflow to the counter */
-            wCaptBuf += ( uint32_t )pHandle->OVFCounter * 0x10000uL;
-
-            if ( pHandle->OVFCounter != 0u )
-            {
-                /* Adjust the capture using prescaler */
-                uint16_t hAux;
-                hAux = hPrscBuf + 1u;
-                wCaptBuf *= hAux;
-            }
-            else
-            {
-                /* If prsc preload reduced in last capture, store current register + 1 */
-                if ( pHandle->RatioDec ) /* and don't decrease it again */
-                {
-                    /* Adjust the capture using prescaler */
-                    uint16_t hAux;
-                    hAux = hPrscBuf + 2u;
-                    wCaptBuf *= hAux;
-
-                    pHandle->RatioDec = false;
-                }
-                else  /* If prescaler was not modified on previous capture */
-                {
-                    /* Adjust the capture using prescaler */
-                    uint16_t hAux = hPrscBuf + 1u;
-                    wCaptBuf *= hAux;
-                }
-            }
-
-#if 0
-
-            /* Store into the buffer */
-            /* Null Speed is detected, erase the buffer */
-            if ( wCaptBuf > pHandle->MaxPeriod )
-            {
-                uint8_t bIndex;
-
-                for ( bIndex = 0u; bIndex < pHandle->SpeedBufferSize; bIndex++ )
-                {
-                    pHandle->SensorSpeed[bIndex]  = 0;
-                }
-
-                pHandle->BufferFilled = 0 ;
-                pHandle->SpeedFIFOSetIdx = 1;
-                pHandle->SpeedFIFOGetIdx = 0;
-                /* Indicate new speed acquisitions */
-                pHandle->NewSpeedAcquisition = 1;
-                pHandle->ElSpeedSum = 0;
-            }
-            /* Filtering to fast speed... could be a glitch  ? */
-            /* the HALL_MAX_PSEUDO_SPEED is temporary in the buffer, and never included in average computation*/
-            else
-#endif
-            if ( wCaptBuf < pHandle->MinPeriod )
-            {
-                pHandle->CurrentSpeed = HALL_MAX_PSEUDO_SPEED;
-                pHandle->NewSpeedAcquisition = 0;
-            }
-            else
-            {
-                pHandle->ElSpeedSum -= pHandle->SensorSpeed[pHandle->SpeedFIFOIdx]; /* value we gonna removed from the accumulator */
-
-                if ( wCaptBuf >= pHandle->MaxPeriod )
-                {
-                    pHandle->SensorSpeed[pHandle->SpeedFIFOIdx]  = 0;
-                }
-                else
-                {
-                    pHandle->SensorSpeed[pHandle->SpeedFIFOIdx] = ( int16_t ) ( pHandle->PseudoFreqConv / wCaptBuf );
-                    pHandle->SensorSpeed[pHandle->SpeedFIFOIdx] *= pHandle->Direction;
-                    pHandle->ElSpeedSum += pHandle->SensorSpeed[pHandle->SpeedFIFOIdx];
-                }
-
-                /* Update pointers to speed buffer */
-                pHandle->CurrentSpeed = pHandle->SensorSpeed[pHandle->SpeedFIFOIdx];
-                pHandle->SpeedFIFOIdx++;
-
-                if ( pHandle->SpeedFIFOIdx == pHandle->SpeedBufferSize )
-                {
-                    pHandle->SpeedFIFOIdx = 0u;
-                }
-
-                /* Indicate new speed acquisitions */
-                pHandle->NewSpeedAcquisition = 1;
-            }
-
-            /* Reset the number of overflow occurred */
-            pHandle->OVFCounter = 0u;
-        }
-    }
-
-    return MC_NULL;
 }
 
 #if defined (CCMRAM)
